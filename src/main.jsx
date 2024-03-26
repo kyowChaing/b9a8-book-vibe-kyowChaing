dd969bcfed8b39c9ddd64d7c78c373d45166001a
Kyow <chakkek15@gmail.com>
2024-03-26 10:15:26 +0600

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+
 import './index.css'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      // {
+      //   path:'/',
+      //   element:<Home></Home>
+      // }
+    ]
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

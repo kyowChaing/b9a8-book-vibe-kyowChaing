4f55c6dfb9f18def3060eac70f33c69d4437cf1d
Kyow <chakkek15@gmail.com>
2024-03-26 10:47:58 +0600

    added banner section

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+import hobbit from '../../assets/hobbit.jpg'
+import './banner.css'
+
+function Banner() {
+  return (
+    <>
+    <div className='bgimg bg-[#F3F3F3] flex justify-around items-center'>
+            <div>
+              <h3 className=' text-black'> Books to freshen <br /> up your bookshelf</h3>
+              <Link> <button className='bbtn p-2 m-3 ' type="">View the List</button></Link>
+            </div>
+            <div className=' my-5'>
+              <img src={hobbit} alt="" />
+            </div>
+    </div>
+    </>
+  )
+}
+
+export default Banner
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
+import Banner from '../Banner/Banner';
+
 
 const Root = () => {
     return (
         <>
             <Navbar> </Navbar>
+            <Banner></Banner>
             <Outlet></Outlet>
         </>
     );

05a549edacae2a6509ecd4ad6fa03b82dddf477b
Kyow <chakkek15@gmail.com>
2024-03-29 11:41:15 +0600

    added read books sort by ratin, number of pages, publishe year

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -3,11 +3,27 @@ import { getStoredBooksId } from "../../utilitis/storeBookId";
 import { getStoredWishList } from "../../utilitis/storeWishList";
 import ReadsBooks from "../ReadsBooks/ReadsBooks";
 import WishList from "../WishList/WishList";
+import dropdownicon from "../../../public/dropdown.png"
 
 const ListedBooks = () => {
   const [books, setBooks] = useState([]);
   const [wishList, setWishList] = useState([]);
-  const [displayBook,setDisplayBook]=useState([]);
+
+  // const [displayBook,setDisplayBook]=useState([]);
+  const handleBookFilter=(filter)=>{
+    // console.log(filter)
+    
+    const sortData = [...books].sort((a,b)=>{
+      if(a[filter]<b[filter]){
+        return 1;
+      }
+      if(a[filter]>b[filter]){
+        return -1;
+      }
+      return 0;
+    });
+    setBooks(sortData);
+  }
 
   useEffect(() => {
     const storedBookIds = getStoredBooksId();
@@ -50,15 +66,15 @@ const ListedBooks = () => {
         <div className=" flex justify-center "> Books </div>
         <div className="flex justify-center">
           <details className="dropdown">
-            <summary className="m-1 btn">Sort By</summary>
+            <summary className="m-1 btn">Sort By <span><img className=" w-4 h-4" src={dropdownicon} alt="" /></span></summary>
             <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-              <li onClick={()=>handleBookFilter(rating)}>
+              <li onClick={()=>handleBookFilter("rating")}>
                 <a>Rating</a>
               </li>
-              <li onClick={()=>{handleBookFilter(totalPages)}}>
+              <li onClick={()=>{handleBookFilter("totalPages")}}>
                 <a>Number of Pages</a>
               </li>
-              <li onClick={()=> handleBookFilter(yearOfPublishing)}>
+              <li onClick={()=> handleBookFilter("yearOfPublishing")}>
                 <a>Published Year</a>
               </li>
             </ul>
@@ -80,7 +96,7 @@ const ListedBooks = () => {
         >
           {books.map((book) => (
             <ReadsBooks key={book.bookId} book={book}>
-              {" "}
+    
             </ReadsBooks>
           ))}
         </div>
@@ -98,7 +114,7 @@ const ListedBooks = () => {
         >
           {wishList.map((book) => (
             <WishList key={book.bookId} book={book}>
-              {" "}
+            
             </WishList>
           ))}
         </div>
--- src/components/PagesToRead/PagesToRead.jsx
+++ src/components/PagesToRead/PagesToRead.jsx
@@ -25,51 +25,7 @@ const PagesToRead = () => {
 
 
     const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
-    
-    const data = [
-      // {
-      //   name: `${books[0].bookName}`,
-      //   uv:`${books[0].totalPages}`,
-      //   pv: 2400,
-      //   amt: 2400,
-      // },
-      {
-        name: 'The Catcher in the Rye',
-        uv: 300,
-        pv: 1398,
-        amt: 2210,
-      },
-      {
-        name: '1984',
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
-      },
-      {
-        name: 'To Kill a Mockingbird',
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
-      },
-      {
-        name: 'The Great Gatsby',
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
-      },
-      {
-        name: 'Sapiens: A Brief History of Humankind',
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
-      },
-    //   {
-    //     name: 'Page G',
-    //     uv: 3490,
-    //     pv: 4300,
-    //     amt: 2100,
-    //   },
-    ];
+    const data =[];
     
     const getPath = (x, y, width, height) => {
       return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}

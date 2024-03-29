c590a188723b712188f057b00892bce4c288dcf4
Kyow <chakkek15@gmail.com>
2024-03-29 13:40:20 +0600

    error page design

--- src/components/ErrorElement/ErrorElement.jsx
+++ src/components/ErrorElement/ErrorElement.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorElement = () => {
     return (
-        <div>
-            Errror
+        <>
+        <div className='bg-[#F0F0F0] h-screen flex items-center justify-center'>
+            <div className=' text-center'> 
+            <h1 className=' text-center'> Oops!</h1>
+            <p className=' p-4'>404 - PAGE NOT FOUND</p>
+
+            <Link to="/"> <button className=' bg-[#0145D4] text-[#fff] p-2 rounded-xl'> GO TO HOMEPAGE </button></Link>
+            </div>
+            
+
         </div>
+        </>
+        
     );
 }
 
